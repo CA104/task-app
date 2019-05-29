2035367db126e9220f21c864d74705046e02e799
Carlos <carlos_arellano@hotmail.ca>
2019-05-29 19:48:43 -0400

    filter tasks according to the current date

--- js/main.js
+++ js/main.js
@@ -120,7 +120,14 @@ const showTaskByComplete = (task) => {
 const isTaskToday = (task) => {
   // Check if the year, month and date match this task's start properties
   // Return true if so, otherwise false
-  return true;
+  if(task.start.year == today.getFullYear() && 
+  task.start.month == today.getMonth() +1 &&
+  task.start.date == today.getDate()) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 
